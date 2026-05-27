2ddf5cf84a4f6dfd71ecc8a335f90194867edfd6
Developer <dev@yaml-tools.local>
2026-05-27 16:10:13 +0800

    deploy: GitHub Pages

---
+++ _next/static/APBUUe9ABfYDlCzlnDCTF/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[lang]","\u002F[lang]\u002Fyaml-formatter","\u002F[lang]\u002Fyaml-query","\u002F[lang]\u002Fyaml-to-json"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
